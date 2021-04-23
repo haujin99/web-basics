6188c4f153423f7dbaf2dc15dee79706073968ee
haujin99 <haujin99@gmail.com>
2021-04-23 22:57:33 +0900

    fix header&main video

--- clon_coding/js/main.js
+++ clon_coding/js/main.js
@@ -17,7 +17,7 @@ window.addEventListener("scroll", function() {
         // 대체 로고를 보이게 처리한다.
         logoE2.style.display = 'block'
         // 헤더 높이 변경
-        headerEl.style.height = '100px'
+        headerEl.style.height = '71.38px'
     }
     else {
         // 스크롤 위치가 100px 미만이면,
